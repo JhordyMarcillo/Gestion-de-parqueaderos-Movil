095a2ba8a4391ade83f5b9380a2a975270ea81d6
JhordyMarcillo <jhordypaulsb2@gmail.com>
2026-02-10 00:05:22 -0500

    dockerfile

--- backend/src/main/java/com/espe/parqueaderos/parqueadero/dto/response/JwtResponse.java
+++ backend/src/main/java/com/espe/parqueaderos/parqueadero/dto/response/JwtResponse.java
@@ -1,16 +1,20 @@
 package com.espe.parqueaderos.parqueadero.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private Long id;
     private String email;
     private String rol;
+
+    public JwtResponse(String token, Long id, String email, String rol) {
+        this.token = token;
+        this.id = id;
+        this.email = email;
+        this.rol = rol;
+        this.type = "Bearer";
+    }
 }
